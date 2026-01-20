ab8ad37cf4660db782b8eebbeed603c8bbd83766
MsciejBednarczyk <bednarczyk1611@gmail.com>
2026-01-20 17:42:54 +0100

    Zmieniono JsonStorageService na klasę z serializacją JSON
    Zmieniono interfejs `JsonStorageService` na klasę `JsonStorageService`, która implementuje interfejs `ISaveLoad<CinemaState>`. Dodano statyczne opcje serializacji JSON, umożliwiające zapis i odczyt stanu kina w formacie JSON. Metoda `Save` zapisuje dane do pliku, a metoda `Load` odczytuje dane z pliku i deserializuje je do obiektu `CinemaState`. W przypadku niepowodzenia deserializacji zgłaszany jest wyjątek `InvalidDataException`.

--- Cinema.Application/JsonStorageService.cs
+++ Cinema.Application/JsonStorageService.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Cinema.Application
+namespace Cinema.Application.Storage
 {
-    internal interface JsonStorageService
+    public class JsonStorageService : ISaveLoad<CinemaState>
     {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            WriteIndented = true
+        };
+
+        public void Save(string path, CinemaState data)
+        {
+            var json = JsonSerializer.Serialize(data, Options);
+            File.WriteAllText(path, json);
+        }
+
+        public CinemaState Load(string path)
+        {
+            var json = File.ReadAllText(path);
+            var state = JsonSerializer.Deserialize<CinemaState>(json, Options);
+            if (state is null) throw new InvalidDataException("Could not deserialize cinema state.");
+            return state;
+        }
     }
 }
