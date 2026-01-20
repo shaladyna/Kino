f27532586c1565f5305029b083aa955d51f5cdd5
MKaczkowski07 <mateusz.kaczkowskii@gmail.com>
2026-01-20 11:14:58 +0100

    Dodanie klas w Cinema.Aplication

---
+++ Cinema.Application/CinemaState.cs
@@ -0,0 +1,17 @@
+﻿using Cinema.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Application
+{
+    public class CinemaState
+    {
+        public List<Movie> Movies { get; set; } = new();
+        public List<Screening> Screenings { get; set; } = new();
+        public List<Reservation> Reservations { get; set; } = new();
+        public List<Hall> Halls { get; set; } = new();
+    }
+}
---
+++ Cinema.Application/ISaveLoad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Application
+{
+   public interface ISaveLoad<T>
+    {
+        void Save(string path, T data);
+        T Load(string path);
+    }
+}
